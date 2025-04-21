edcd70ff8df53f9db7b93e5368603ab2e9b7dbbb
jerrymaufler <jerry.maufler@outlook.com>
2025-04-21 18:22:07 +0100

    Add files via upload

---
+++ netlify/functions/fetchPage.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  const { url } = event.queryStringParameters;
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: 'Missing URL'
+    };
+  }
+
+  try {
+    const response = await axios.get(url);
+    return {
+      statusCode: 200,
+      body: response.data,
+      headers: {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: 'Error fetching page: ' + err.message
+    };
+  }
+};
