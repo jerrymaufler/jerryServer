2cd6e9e2a6e789873016cf0b9959bb9b8d50a0ca
jerrymaufler <jerry.maufler@outlook.com>
2025-04-23 14:43:54 +0100

    Update fetchPage.js

--- netlify/functions/fetchPage.js
+++ netlify/functions/fetchPage.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-// ok
+
 exports.handler = async function(event, context) {
   const { url } = event.queryStringParameters;
 
@@ -12,9 +12,15 @@ exports.handler = async function(event, context) {
 
   try {
     const response = await axios.get(url);
+    let html = response.data;
+
+    // Inject <base href="..."> into the <head>
+    const baseHref = `<base href="${url}">`;
+    html = html.replace(/<head([^>]*)>/i, `<head$1>${baseHref}`);
+
     return {
       statusCode: 200,
-      body: response.data,
+      body: html,
       headers: {
         'Content-Type': 'text/html',
         'Access-Control-Allow-Origin': '*'
